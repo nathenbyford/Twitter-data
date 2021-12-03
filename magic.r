559c89e833ce82041f4d580f455f163855ef9ed6
nathenbyford <Nathenbyford@gmail.com>
2021-12-02 21:12:09 -0600

    question 4

---
+++ magic.r
@@ -0,0 +1,26 @@
+library("tidyverse")
+theme_set(
+  theme_classic() +
+    theme(panel.grid.major.y = element_line(),
+          axis.title.y = element_text(angle = 0, vjust = .5)
+    )
+)
+
+dtweets <- read_rds(".//data//dtweets.rds")
+
+## Question 4.
+
+mentions <- dtweets |> 
+  pull(text) |> 
+  str_extract_all(pattern = "\\@\\w{4,15}") |> 
+
+
+mentions
+
+map(mentions, str_split, pattern = ", ") |> 
+  unlist() |> 
+  as_tibble() |> 
+  group_by(value) |> 
+  count() |> 
+  ungroup() |> 
+  arrange(desc(n))
